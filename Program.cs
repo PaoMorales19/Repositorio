88c7482708f3005d5af6c471216a3b0070377730
PaoMorales19 <ap.morales.anaya@gmail.com>
2022-09-12 09:18:00 -0500

    Comit prueba

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace Evalua
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Lenguaje a = new Lenguaje();//C:\\Users\\HOME\\Documents\\PAOLA TAREAS\\5TO SEMESTRE\\AUTOMATAS II\\UNIDAD 1\\Evalua\\prueba.cpp"
+
+                a.Programa();
+                /*while(!a.FinArchivo())
+                {
+                    a.NextToken();
+                }*/
+                a.cerrar();
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
